dd5c4ab8bb3bc5030f5679fef2efc48ea4f9f383
Mariano D'Agostino <dagostinoips@gmail.com>
2019-04-09 17:07:20 -0300

    Valido que lo que ingresé sea un numero.

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 
 $numero = readline("Ingrese un numero");
 
+if (!is_numeric($numero)) {
+  echo "Error. No ingresaste un numero\n";
+  exit();
+}
+
 if ($numero % 2 == 0) {
   echo "Es par\n";
 }
